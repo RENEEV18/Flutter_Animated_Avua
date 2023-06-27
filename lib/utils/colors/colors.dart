1cb1e35128640d35211136d0cf822337388a0701
RENEEV18 <reneevraj3@gmail.com>
2023-06-27 19:15:14 +0530

    Initial Commit

---
+++ lib/utils/colors/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const baseColor = Color.fromARGB(255, 132, 1, 155);
+  static const appBarColor = Color.fromARGB(255, 78, 0, 92);
+}
